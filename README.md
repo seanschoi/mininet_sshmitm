83528f2fb82e46052c834b75dedae83c1de93fd8
Sean Choi <yo2seol@stanford.edu>
2015-08-23 18:28:23 -0700

    Initial commit. Example to run ssh man in the middle attack using miminet and ettercap

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # mininet_sshmitm
+1. Install Mininet
+2. Run sshmitm.py
+3. xterm h1 h2 h3
+4. [h3]: echo 1 > /proc/sys/net/ipv4/ip_forward, cat /proc/sys/net/ipv4/ip_forward
+5. [h3]: ettercap -G --filter etter_filter_ssh_co
+6. Check option only checks 'Promisc Mode'
+7. Sniffing -> Unified Sniffing -> h3-eth0
+8. Hosts->Scan for Hosts
+9. Hosts -> Host List -> 192.168.0.3 Add to target 1, 192.168.0.4 Add to target 2.
+10. Check. Targets -> Current Targets
+11. Mitm -> ARP Spoofing -> Sniff Remote Connections check -> Press OK, Check log for group
+12. Start -> Start Sniffing
+13. h1: ssh -1 mininet@192.168.0.4
+14. If 'WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!' shows, ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.0.4
+retry
+15. See both '[SSH Filter] SSH downgraded from version 2 to 1' and 'SSH : 192.168.0.4:22 -> USER: mininet  PASS: mininet' in the ettercap conole
+16. Any decoded will be in decoded.log, and undecoded in logfile.log. Tail -f in decoded.log to see the updates
---
+++ sshmitm.py
@@ -0,0 +1,99 @@
+"""
+Custom topology that creates 3 hosts under a single switch.
+Use for examples such as SSH MITM attack and TCP Hijacking
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.net import Mininet
+from mininet.node import Node, Controller, RemoteController
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+from mininet.topo import Topo
+from mininet.util import quietRun
+from mininet.moduledeps import pathCheck
+
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
+
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd("sysctl -w net.ipv4.ip_forward=1")
+        self.cmd("sysctl -p /etc/sysctl.conf")
+
+    def terminate( self ):
+        self.cmd("sysctl -w net.ipv4.ip_forward=0")
+        self.cmd("sysctl -p /etc/sysctl.conf")
+        super( LinuxRouter, self ).terminate()
+
+class AttackTopo( Topo ):
+    "Simple topology example."
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        left_host = self.addHost( 'h1', ip='192.168.0.3/24',
+            mac='00:00:00:00:00:01')
+        right_host = self.addHost( 'h2', ip='192.168.0.4/24',
+            mac='00:00:00:00:00:02')
+        #attacker_host = self.addHost( 'h3', ip='192.168.0.5/24',
+        #    mac='00:00:00:00:00:03')
+        attacker_host = self.addNode( 'h3', cls=LinuxRouter, ip='192.168.0.5/24',
+            mac='00:00:00:00:00:03')
+        main_switch = self.addSwitch( 's1' )
+        
+        # Add links from the hosts to this single switch
+        self.addLink( left_host, main_switch )
+        self.addLink( right_host, main_switch )
+        self.addLink( attacker_host, main_switch )
+
+def start_sshd( host ):
+    "Start sshd on host"
+    stop_sshd()
+    info( '*** Starting sshd in %s\n' % host.name )
+    name, intf, ip = host.name, host.defaultIntf(), host.IP()
+    banner = '/tmp/%s.banner' % name
+    host.cmd( 'echo "Welcome to %s at %s" >  %s' % ( name, ip, banner ) )
+    host.cmd( '/usr/sbin/sshd -o Banner=%s -o UseDNS=no' % banner)
+    info( '***', host.name, 'is running sshd on', intf, 'at', ip, '\n' )
+
+def stop_sshd():
+    "Stop *all* sshd processes with a custom banner"
+    info( '*** Shutting down stale sshd/Banner processes ',
+          quietRun( "pkill -9 -f Banner" ), '\n' )
+
+def create_attack_log(host):
+    host.cmd("chmod 666 /home/mininet/sshmitm/decoded.log>>!#:2")
+    host.cmd("chmod 666 /home/mininet/sshmitm/logfile.log>>!#:2")
+
+def delete_attack_log(host):
+    host.cmd("rm /home/mininet/sshmitm/decoded.log")
+    host.cmd("rm /home/mininet/sshmitm/logfile.log")
+
+def main():
+    topo = AttackTopo()
+    info( '*** Creating network\n' )
+    net = Mininet(topo=topo)
+    net.start()
+    print net.items()
+    h1, h2, attacker, s1 = net.get('h1', 'h2', 'h3', 's1')
+    # Start a ssh server on host 2
+    start_sshd(h2)
+    create_attack_log(attacker)
+    CLI(net)
+    stop_sshd()
+    delete_attack_log(attacker)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    main()
+
+
+#topos = { 'attacktopo': ( lambda: AttackTopo() ) }
